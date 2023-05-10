bcbe6104e64919fb5c0f09568c3155870042cdd3
jam sadiq <jsadiq@sissa.it>
2023-05-10 11:05:50 +0200

    minor modifications

--- pythoncode/utils_implicit.py
+++ pythoncode/utils_implicit.py
@@ -2,6 +2,25 @@
 
 import numpy as np
 
+def TDMA(a,b,c,d):
+    n = len(d)
+    w= np.zeros(n-1,float)
+    g= np.zeros(n, float)
+    p = np.zeros(n,float)
+    
+    w[0] = c[0]/b[0]
+    g[0] = d[0]/b[0]
+
+    for i in range(1,n-1):
+        w[i] = c[i]/(b[i] - a[i-1]*w[i-1])
+    for i in range(1,n):
+        g[i] = (d[i] - a[i-1]*g[i-1])/(b[i] - a[i-1]*w[i-1])
+    p[n-1] = g[n-1]
+    for i in range(n-1,0,-1):
+        p[i-1] = g[i-1] - w[i-1]*p[i]
+    return p
+
+
 def TDMAsolver(a, b, c, d):
     """
     a b c d can be NumPy array type or Python list type.
@@ -26,17 +45,19 @@ def TDMAsolver(a, b, c, d):
     return xc
 
 #test TDMA
-A = np.array([[10, 2, 0, 0],[3, 10, 4, 0], [0, 1, 7, 5], [0, 0, 3, 4]], dtype=float)
-d = np.array([3, 4, 5, 6.])
-#solve via linear algebra
-sol1 = np.linalg.solve(A, d)
-#Now TDMA solver
-a = np.array([3., 1., 3.]) #below the diagonal
-b = np.array([10., 10., 7., 4.])
-c = np.array([2., 4., 5.]) #above the diagonal
-sol2 = TDMAsolver(a, b, c, d)
-print(sol1, sol2)
-
+#A = np.array([[10, 2, 0, 0],[3, 10, 4, 0], [0, 1, 7, 5], [0, 0, 3, 4]], dtype=float)
+#d = np.array([3, 4, 5, 6.])
+##solve via linear algebra
+#sol1 = np.linalg.solve(A, d)
+##Now TDMA solver
+#a = np.array([3., 1., 3.]) #below the diagonal
+##b = np.array([10., 10., 7., 4.])
+#c = np.array([2., 4., 5.]) #above the diagonal
+#sol2 = TDMAsolver(a, b, c, d)
+#sol3 = TDMA(a, b, c, d)
+#print(sol1, sol2, sol3)
+#quit()
+#
 #Inverse first we need tridaigonal Matrix and its easy to get its invers and use A-1*d with np.dot should give what solver do
 
 def TridiagonalMatrix(size_of_a_matrix, diagonalVal, diagonalAboveVal, diagonalBelowVal):
@@ -57,4 +78,4 @@ def TridiagonalMatrix(size_of_a_matrix, diagonalVal, diagonalAboveVal, diagonalB
     return matrix # "this is the tridiagonal matrix"
 
 #print(tridiagonal(5, 6, -2, -1))
-
+#scipy.linalg.solve_triangular(a, b)
